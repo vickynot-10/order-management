a4800acb4475d1126cc3769eebe294493763c6d3
vickynot-10 <brianvickyx@gamil.com>
2026-08-03 22:49:03 +0530

    dev

--- hooks/queries/useOrders.ts
+++ hooks/queries/useOrders.ts
@@ -25,7 +25,9 @@ export function useGetOrders(deviceId: string) {
   return useQuery({
     queryKey: ["orders", deviceId],
     queryFn: async () => {
-      const res = await api.post("/orders", { device_id: deviceId });
+      const res = await api.get("/orders", { params : {
+        device_id: deviceId
+      } });
       return res.data;
     },
     enabled: !!deviceId,
